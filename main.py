01d684b89e0ec0fc42ca33a09fed96c2c75f5df9
JanLohse <janlohse@mail.uni-paderborn.de>
2025-03-05 15:36:59 +0100

    reformat

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import os
-import sys
-import traceback
-from pathlib import Path
-
 import ffmpeg
 import numpy as np
-from PyQt6.QtCore import QSize, Qt, QObject, pyqtSignal, QRunnable, pyqtSlot, QThreadPool
+from PyQt6.QtCore import QSize, QThreadPool
 from PyQt6.QtGui import QPixmap, QIntValidator
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QWidget, QHBoxLayout, QComboBox, \
-    QFileDialog, QLineEdit, QGridLayout, QSizePolicy, QSlider, QCheckBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QComboBox, \
+    QGridLayout, QSizePolicy, QCheckBox
 from colour.models import RGB_COLOURSPACES
 
 from negative_film.kodak_5207 import Kodak5207
@@ -18,65 +13,7 @@ from print_film.kodak_2393 import Kodak2393
 from print_film.kodak_endura_premier import KodakEnduraPremier
 from reversal_film.fuji_instax_color import FujiInstaxColor
 from reversal_film.kodachrome_64 import Kodachrome64
-from spectral_lut import create_lut
-
-
-class WorkerSignals(QObject):
-    """Signals from a running worker thread.
-
-    finished
-        No data
-
-    error
-        tuple (exctype, value, traceback.format_exc())
-
-    result
-        object data returned from processing, anything
-
-    progress
-        float indicating % progress
-    """
-
-    finished = pyqtSignal()
-    error = pyqtSignal(tuple)
-    result = pyqtSignal(object)
-    progress = pyqtSignal(float)
-
-
-class Worker(QRunnable):
-    """Worker thread.
-
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
-    :param callback: The function callback to run on this worker thread.
-                     Supplied args and
-                     kwargs will be passed through to the runner.
-    :type callback: function
-    :param args: Arguments to pass to the callback function
-    :param kwargs: Keywords to pass to the callback function
-    """
-
-    def __init__(self, fn, *args, **kwargs):
-        super().__init__()
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-        # Add the callback to our kwargs
-        self.kwargs["progress_callback"] = self.signals.progress
-
-    @pyqtSlot()
-    def run(self):
-        try:
-            result = self.fn(*self.args, **self.kwargs)
-        except Exception:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-        else:
-            self.signals.result.emit(result)
-        finally:
-            self.signals.finished.emit()
+from utils import *
 
 
 class MainWindow(QMainWindow):
@@ -292,85 +229,6 @@ class MainWindow(QMainWindow):
 
         if ok:
             self.generate_lut(filename, int(self.lut_size.currentText()))
-
-
-class FileSelector(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        layout = QHBoxLayout()
-        self.setLayout(layout)
-        layout.setContentsMargins(0, 0, 0, 0)
-
-        file_browse = QPushButton('Browse')
-        file_browse.setFixedWidth(55)
-        file_browse.clicked.connect(self.open_file_dialog)
-        self.filename_edit = QLineEdit()
-
-        layout.addWidget(self.filename_edit)
-        layout.addWidget(file_browse)
-
-        self.textChanged = self.filename_edit.textChanged
-
-        self.show()
-
-    def open_file_dialog(self):
-        filename, ok = QFileDialog.getOpenFileName(self, "Select a File", "D:\\icons\\avatar\\",
-                                                   "Images (*.png *.jpg *)")
-        if filename:
-            path = Path(filename)
-            self.filename_edit.setText(str(path))
-
-    def currentText(self):
-        return self.filename_edit.text()
-
-
-class Slider(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        layout = QHBoxLayout()
-        self.setLayout(layout)
-        layout.setContentsMargins(0, 0, 0, 0)
-
-        self.slider = QSlider()
-        self.slider.setOrientation(Qt.Orientation.Horizontal)
-        self.setMinMaxTicks(0, 1, 1)
-
-        self.text = QLabel()
-        self.text.setAlignment(Qt.AlignmentFlag.AlignRight)
-        self.text.setFixedWidth(30)
-
-        layout.addWidget(self.slider)
-        layout.addWidget(self.text)
-
-        self.slider.valueChanged.connect(self.sliderValueChanged)
-
-        self.valueChanged = self.slider.valueChanged
-
-    def setMinMaxTicks(self, min, max, enumerator=1, denominator=1):
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(int(((max - min) * denominator) / enumerator))
-        self.min = min
-        self.enumerator = enumerator
-        self.denominator = denominator
-
-    def sliderValueChanged(self):
-        value = self.getValue()
-        if value.is_integer():
-            self.text.setText(str(int(value)))
-        else:
-            self.text.setText(f"{value:.2f}")
-
-    def getValue(self):
-        return self.slider.value() * self.enumerator / self.denominator + self.min
-
-    def setValue(self, value):
-        self.slider.setValue(round((value - self.min) * self.denominator / self.enumerator))
-
-    def setPosition(self, position):
-        self.slider.setValue(position)
-
-    def getPosition(self):
-        return self.slider.value()
 
 
 if __name__ == '__main__':
